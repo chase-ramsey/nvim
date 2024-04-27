f231ea4a78d91fd4f8463a614256d3dd534ef4b7
Chase Ramsey <ramsey.chase@gmail.com>
2024-04-27 00:03:17 -0400

    basic lsp setup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,20 @@ return {
       vim.keymap.set('n', '<leader>fb', ts_builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', ts_builtin.help_tags, {})
     end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      }
+      require("telescope").load_extension("ui-select")
+    end
   }
 }
 
