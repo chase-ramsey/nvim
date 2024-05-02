8bfcd989ec59b9dbde06e5e25ba8579cbff06355
Chase Ramsey <ramsey.chase@gmail.com>
2024-05-02 08:51:31 -0400

    temp

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,33 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "rcarriga/nvim-dap-ui",
+    "mfussenegger/nvim-dap-python"
+  },
+  config = function()
+    local dappy = require("dap-python")
+    dappy.setup("~/.pyenv/versions/3.9.13/bin/python")
+    dappy.test_runner = "pytest"
+
+    local dap = require("dap")
+    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+    vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Debug continue" })
+
+    local dapui = require("dapui")
+    dapui.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+  end
+}
