85505ad06cca4af734759bf92d78efb069c7341f
Chase Ramsey <ramsey.chase@gmail.com>
2024-05-13 17:04:58 -0400

    Add copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+  "github/copilot.vim",
+  -- config = function()
+  --   local copilot = require("github-copilot")
+  --   copilot.setup({
+  --     no_tab_map = true,
+  --   })
+  --   vim.keymap.set("i", "<<S-Tab>>", copilot.AcceptSuggestion, { desc = "Accept Copilot suggestion" })
+  -- end
+}
