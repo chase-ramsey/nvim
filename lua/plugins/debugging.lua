b44e7f920bed1b4dba3daebb06bd7d66168a5511
Chase Ramsey <ramsey.chase@gmail.com>
2024-05-22 16:56:06 -0400

    Updates to debugging config

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -7,16 +7,26 @@ return {
   },
   config = function()
     local dap = require("dap")
-    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
-    vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Debug continue" })
-    vim.keymap.set("n", "<Leader>ds", dap.step_over, { desc = "Debug step over" })
-    vim.keymap.set("n", "<Leader>dj", dap.step_into, { desc = "Debug step into" })
-    vim.keymap.set("n", "<Leader>dk", dap.step_out, { desc = "Debug step out" })
+    vim.keymap.set("n", "<C-b>", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+    vim.keymap.set("n", "<C-d>", dap.continue, { desc = "Debug continue" })
+    vim.keymap.set("n", "<Leader>s", dap.step_over, { desc = "Debug step over" })
+    vim.keymap.set("n", "<Leader>j", dap.step_into, { desc = "Debug step into" })
+    vim.keymap.set("n", "<Leader>k", dap.step_out, { desc = "Debug step out" })
 
     local dappy = require("dap-python")
     dappy.setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
+
+    local function _wrap_test_method_with_opts()
+      dappy.test_method({ config = { justMyCode = false } })
+    end
+
+    local function _wrap_test_class_with_opts()
+      dappy.test_class({ config = { justMyCode = false } })
+    end
+
     dappy.test_runner = "pytest"
-    vim.keymap.set("n", "<Leader>dm", dappy.test_method, { desc = "Python: test method (debug)" })
+    vim.keymap.set("n", "<Leader>dm", _wrap_test_method_with_opts, { desc = "Python: test method (debug)" })
+    vim.keymap.set("n", "<Leader>dc", _wrap_test_class_with_opts, { desc = "Python: test class (debug)" })
 
     local dapui = require("dapui")
     dapui.setup()
