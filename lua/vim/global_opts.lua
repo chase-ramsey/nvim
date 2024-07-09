61e0ae33fcac8ac67e8944ff36ab26f4e6cff4e1
Chase Ramsey <ramsey.chase@gmail.com>
2024-07-09 17:13:37 -0400

    Set filetype for html template files

--- lua/vim/global_opts.lua
+++ lua/vim/global_opts.lua
@@ -6,3 +6,10 @@ vim.o.expandtab = true  -- use spaces for tabs
 vim.o.tabstop = 2       -- length of a tab defined in spaces
 vim.o.softtabstop = 2   -- length in spaces inserted on press of <Tab> key
 vim.o.shiftwidth = 2    -- number of spaces for an indent
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.html*",
+  callback = function()
+    vim.cmd("setlocal filetype=html")
+  end
+})
