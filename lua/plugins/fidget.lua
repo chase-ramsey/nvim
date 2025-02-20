60b2424a27f28a751988548f02e154fbd6f1043b
Chase Ramsey <ramsey.chase@gmail.com>
2025-02-20 09:00:42 -0500

    Add :Fidget history shortcut

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -4,5 +4,14 @@ return {
     notification = {
       override_vim_notify = true,
     }
-  }
+  },
+  config = function()
+    require("fidget").setup({
+      notification = {
+        override_vim_notify = true,
+      }
+    })
+
+    vim.api.nvim_set_keymap("n", "<C-f>h", "<cmd>Fidget history<cr>", { desc = "Show Fidget history" })
+  end,
 }
